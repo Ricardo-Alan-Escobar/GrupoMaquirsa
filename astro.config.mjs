2ab79f7668b486007ffaabdafb8255614adaeb7c
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2024-09-24 10:09:01 -0600

    estilos actualizados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://Ricardo-Alan-Escobar.github.io',
-  base:'/GrupoMaquirsa',
-  devOptions: {
-    devToolbar: false,
-  },
-  integrations: [tailwind()]
+  base: '/GrupoMaquirsa',
+  integrations: [tailwind()],
 });
